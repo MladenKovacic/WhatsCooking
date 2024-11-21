2c7446e8d5d15114599a3eee66c131fd9611a162
MladenKovacic <mladen.kovacic406@gmail.com>
2024-11-21 15:12:30 +0100

    added some stuff

---
+++ script.js
@@ -0,0 +1,12 @@
+
+const openWhatFood = document.getElementById("whatFood")
+const closeModal = document.getElementById("closeModal")
+const modalContainer = document.getElementById("modalcontainer")
+
+
+
+
+
+function openWhatFood() {
+ modalContainer.classList.remove
+}
